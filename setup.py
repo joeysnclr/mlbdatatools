386bf1e61c6bf695ce3b71e1ea8b3c2e1bc6932f
Joseph Sinclair <joey.sinclair02@gmail.com>
2024-11-19 22:19:07 -0800

    readme for pypi page

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
 
 setup(
     name='mlbdatatools',
@@ -8,4 +10,6 @@ setup(
     description='DataFrames, type-safety, and plotting for modern baseball analytics.',
     author='Joey Sinclair',
     install_requires=['pandas', 'numpy', 'matplotlib', 'requests'],
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
 )
