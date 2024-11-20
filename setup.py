e0267521949a7cfca7d4d8235be654fed753437e
Joseph Sinclair <joey.sinclair02@gmail.com>
2024-11-19 22:17:42 -0800

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='mlbdatatools',
     packages=find_packages(include=['mlbdatatools']),
     url='https://github.com/joeysnclr/mlbdatatools',
-    version='0.1.2',
+    version='0.1.3',
     description='DataFrames, type-safety, and plotting for modern baseball analytics.',
     author='Joey Sinclair',
     install_requires=['pandas', 'numpy', 'matplotlib', 'requests'],
